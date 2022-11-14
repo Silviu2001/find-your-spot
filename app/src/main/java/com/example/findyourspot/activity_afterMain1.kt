6d7020c71236b397491850282c2637ca5bb43588
Edina Novak <edina.novak0@gmail.com>
2022-11-14 21:13:16 +0200

    add new activity - Edina

---
+++ app/src/main/java/com/example/findyourspot/activity_afterMain1.kt
@@ -0,0 +1,11 @@
+package com.example.findyourspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_afterMain1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_main1)
+    }
+}
